28956b5fdfabbf8d480d4b925953486033a61752
RicBatista <ricardobatistarg@gmail.com>
2026-01-05 13:34:44 -0300

    teste

--- src/main/java/br/com/jrnb/webflux/model/request/UserRequest.java
+++ src/main/java/br/com/jrnb/webflux/model/request/UserRequest.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-import java.io.Serializable;
-
 /**
  * DTO for {@link br.com.jrnb.webflux.entity.User}
  */
---
+++ src/test/java/br/com/jrnb/webflux/service/UserServiceTest.java
@@ -0,0 +1,52 @@
+package br.com.jrnb.webflux.service;
+
+import br.com.jrnb.webflux.entity.User;
+import br.com.jrnb.webflux.mapper.UserMapper;
+import br.com.jrnb.webflux.model.request.UserRequest;
+import br.com.jrnb.webflux.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @Test
+    void save() {
+        UserRequest request = new UserRequest("", "", "");
+        User entity = User.builder().build();
+
+        when(userMapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(userRepository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = userService.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(Objects::nonNull)
+                .expectComplete()
+                .verify();
+
+        assertNotNull(request.name());
+
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+}
